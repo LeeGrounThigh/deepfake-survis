6c4d03716682480f431f4504231418343aca865c
sET <127002398+LeeGrounThigh@users.noreply.github.com>
2025-05-11 17:33:30 +0100

    Update tag_categories.js

--- docs/data/tag_categories.js
+++ docs/data/tag_categories.js
@@ -1,8 +1,5 @@
 const userDefinedTagCategories = {
-  "type": {
-    "description": "Detection model type or paper form"
-  },
-  "other": {
-    "description": "Input type, dataset, evaluation, etc."
-  }
-};
+	"type": {
+		"description": "type of the paper"
+	}
+}
