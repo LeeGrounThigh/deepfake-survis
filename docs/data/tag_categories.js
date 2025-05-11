3bb27f06246763d323cdd52d096a7c068fc63ba0
sET <127002398+LeeGrounThigh@users.noreply.github.com>
2025-05-11 16:10:59 +0100

    Update tag_categories.js

--- docs/data/tag_categories.js
+++ docs/data/tag_categories.js
@@ -1,11 +1,5 @@
 const userDefinedTagCategories = {
-  "Model": {
-    "description": "Model architecture type"
-  },
-  "Input": {
-    "description": "Input data type"
-  },
-  "Eval": {
-    "description": "Evaluation method"
-  }
+  "Model": { "description": "Model architecture type" },
+  "Input": { "description": "Input data type" },
+  "Eval":  { "description": "Evaluation method" }
 };
