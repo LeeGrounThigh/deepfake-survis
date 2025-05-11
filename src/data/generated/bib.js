2e30844810a7424b2d15b77d511b02ad10683aae
sET <127002398+LeeGrounThigh@users.noreply.github.com>
2025-05-11 14:56:49 +0100

    Add files via upload

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,93 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+    "Jaiswal2021": {
+      "title": "Hybrid Recurrent Deep Learning Model for DeepFake Video Detection",
+      "author": "Gaurav Jaiswal",
+      "year": "2021",
+      "doi": "10.1109/UPCON52273.2021.9667632",
+      "type": "inproceedings",
+      "publisher": "IEEE",
+      "venue": "IEEE UPCON"
+    },
+    "Nguyen2024": {
+      "title": "Exploring Self-Supervised Vision Transformers for Deepfake Detection",
+      "author": "Nguyen, H. H.; Yamagishi, J.; Echizen, I.",
+      "year": "2024",
+      "doi": "10.48550/arXiv.2405.00355",
+      "type": "article",
+      "publisher": "arXiv",
+      "venue": "arXiv"
+    },
+    "Wodajo2021": {
+      "title": "Deepfake Detection Using Convolutional Vision Transformer",
+      "author": "Wodajo, D.; Atnafu, S.",
+      "year": "2021",
+      "doi": "10.48550/arXiv.2102.11126",
+      "type": "article",
+      "publisher": "arXiv",
+      "venue": "arXiv"
+    },
+    "Soudy2024": {
+      "title": "Deepfake Detection Using Convolutional Vision Transformers and CNNs",
+      "author": "Soudy, A. H.; Sayed, O.; Tag-Elser, H.; et al.",
+      "year": "2024",
+      "doi": "10.1007/s00521-024-10181-7",
+      "type": "article",
+      "publisher": "Neural Computing and Applications",
+      "venue": "NCA"
+    },
+    "Kosarkar2024": {
+      "title": "A Hybrid Deep Learning Model for Robust Deepfake Detection",
+      "author": "Kosarkar, U. P.; Sakarkar, G.; Naik, M.",
+      "year": "2024",
+      "doi": "10.1007/978-981-97-6222-4_9",
+      "type": "inproceedings",
+      "publisher": "Springer",
+      "venue": "AICMI"
+    },
+    "Liao2024": {
+      "title": "A New Deepfake Detection Method by Vision Transformers",
+      "author": "Liao, M.; Chen, M.",
+      "year": "2024",
+      "doi": "10.1117/12.3051840",
+      "type": "inproceedings",
+      "publisher": "SPIE",
+      "venue": "AHPCIAI"
+    },
+    "Li2025": {
+      "title": "Survey on Technologies of Video Deepfake Detection",
+      "author": "Li, C.; Song, Z.; Liang, Y.; Wang, X. A.",
+      "year": "2025",
+      "doi": "10.1007/978-3-031-86149-9_18",
+      "type": "inproceedings",
+      "publisher": "Springer",
+      "venue": "EIDWT"
+    },
+    "Luan2025": {
+      "title": "A Survey on Deepfake Detection Technologies",
+      "author": "Luan, T. A. O.",
+      "year": "2025",
+      "doi": "10.62677/IJETAA.2501131",
+      "type": "article",
+      "publisher": "IJETAA",
+      "venue": "IJETAA"
+    },
+    "Makwana2025": {
+      "title": "Emerging Innovations in Deep Learning for Video DeepFake Detection: A Comprehensive Review",
+      "author": "Makwana, M. K.; Singh, D. K.; Shukla, S.",
+      "year": "2025",
+      "doi": "10.1109/ICSADL65848.2025.10933018",
+      "type": "inproceedings",
+      "publisher": "IEEE",
+      "venue": "ICSADL"
+    },
+    "Rana2022": {
+      "title": "Deepfake Detection: A Systematic Literature Review",
+      "author": "Rana, M. S.; Nobi, M. N.; Murali, B.; Sung, A. H.",
+      "year": "2022",
+      "doi": "10.1109/ACCESS.2022.3154404",
+      "type": "article",
+      "publisher": "IEEE Access",
+      "venue": "IEEE Access"
     }
-};
+  };
+  
